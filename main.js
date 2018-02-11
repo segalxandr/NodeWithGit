c64902498228f4d96e0438aa1b362584b5e1750b
alexsegal <asegal2012@gmail.com>
2018-02-11 13:37:52 -0500

    Starting to follow

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ console.log("Running webapp...");
 
 let Express = require('express');
 let app = new Express();
-let port = process.env.PORT || 80;
+let port = process.env.PORT || 3001;
 app.use(require('body-parser').urlencoded());
 app.use(require('body-parser').json());
 
 app.use(Express.static('./app'));
-app.get('/messages',(req,res)=>{
-	res.status(200).json(require('./messages.json'))
+app.get('/messages', (req, res) => {
+    res.status(200).json(require('./messages.json'))
 })
-app.listen(port,()=>{
-	//console.log(`Running webhook listener...`);
-	console.log(`App listening on port ${port}`);
-}) ;
+app.listen(port, () => {
+    //console.log(`Running webhook listener...`);
+    console.log(`App listening on port ${port}`);
+});
